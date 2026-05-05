9f705451aaa8f8e9bdd682cc72504b2f0e1948f9
Kalle <kalle@kakmonstret.se>
2026-05-05 16:33:03 +0200

    hela jäkla skiten

--- lib/pages/main_view.dart
+++ lib/pages/main_view.dart
@@ -1,38 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:imat_app/app_theme.dart';
-import 'package:imat_app/model/imat_data_handler.dart';
-import 'package:imat_app/widgets/product_card.dart';
-import 'package:provider/provider.dart';
+import '../widgets/category_panel.dart';
+import '../widgets/product_grid.dart';
+import '../widgets/shopping_cart_panel.dart';
 
 class MainView extends StatelessWidget {
   const MainView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var iMat = context.watch<ImatDataHandler>();
-    var products = iMat.selectProducts;
-
-    // Det finns en version utan gridDelegate nedan.
-    // Den kan vara enklare att förstå.
-    // Denna version har fördelen att kort skapas on-demand.
     return Scaffold(
-      appBar: AppBar(title: const Text('iMats produkter')),
-      body: Padding(
-        padding: const EdgeInsets.all(AppTheme.paddingSmall),
-        child: GridView.builder(
-          itemCount: products.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 4, // 4 kolumner
-            crossAxisSpacing: AppTheme.paddingSmall,
-            mainAxisSpacing: AppTheme.paddingSmall,
-            childAspectRatio: 4 / 3,
-          ),
-          itemBuilder: (context, index) {
-            final product = products[index];
-
-            return ProductCard(product, iMat);
-          },
-        ),
+      body: Row(
+        children: [
+          SizedBox(width: 200, child: CategoryPanel()),
+          VerticalDivider(width: 1),
+          Expanded(child: ProductGrid()),
+          VerticalDivider(width: 1),
+          SizedBox(width: 250, child: ShoppingCartPanel()),
+        ],
       ),
     );
   }
